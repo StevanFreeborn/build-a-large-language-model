1fa1ce87ba5be5a270877db30f1d7094e2e4def7
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-03-21 14:57:39 -0500

    feat: work on causal attention mechanism

---
+++ chapter_03/causal_attention_mechanism.py
@@ -0,0 +1,46 @@
+"""A causal attention mechanism implemented with PyTorch."""
+
+import torch
+from self_attention import SelfAttentionV2
+
+inputs = torch.tensor(
+    [
+        [0.43, 0.15, 0.89],
+        [0.55, 0.87, 0.66],
+        [0.57, 0.85, 0.64],
+        [0.22, 0.58, 0.33],
+        [0.77, 0.25, 0.10],
+        [0.05, 0.80, 0.55],
+    ]
+)
+
+x_2 = inputs[1]
+d_in = inputs.shape[1]
+D_OUT = 2
+
+torch.manual_seed(789)
+sa_v2 = SelfAttentionV2(d_in, D_OUT)
+
+queries = sa_v2.w_query(inputs)    
+keys = sa_v2.w_key(inputs) 
+attn_scores = queries @ keys.T
+attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+print(attn_weights)
+
+context_length = attn_scores.shape[0]
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+print(mask_simple)
+
+masked_simple = attn_weights*mask_simple
+print(masked_simple)
+
+row_sums = masked_simple.sum(dim=-1, keepdim=True)
+masked_simple_norm = masked_simple / row_sums
+print(masked_simple_norm)
+
+mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+print(masked)
+
+attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=1)
+print(attn_weights)
